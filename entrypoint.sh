2b17c7f8d6d9cc80e46bea88c7285eca9195f651
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-07-23 12:26:04 +0300

    Измене для новой реализации

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,5 @@ echo "Docker container has been started"
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
-# Настройка расписания планировщика
-echo "SHELL=/bin/bash
-BASH_ENV=/container.env
-$CRON >> /var/log/cron.log 2>&1
-# This extra line makes it a valid cron" > scheduler.txt
-
-crontab scheduler.txt
+crontab scheduler
 cron -f
