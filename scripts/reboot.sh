cee6f1a9dae44bc26b8fb63fb88c85c92b3f663d
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-11 13:30:46 +0300

    Доьбавлен скрипт выполнения скриптов после перезагрузки

---
+++ scripts/reboot.sh
@@ -0,0 +1,5 @@
+for file in /etc/cron.reboot/*
+do 
+    echo "$file" >> /var/log/cron/reboot-$(date +"%Y-%m-%d").log 2>&1; 
+    bash "$file" >> /var/log/cron/reboot-$(date +"%Y-%m-%d").log 2>&1;
+done
