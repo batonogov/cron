345aee698191828f3f470c3da5090227a2e4a81f
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-29 08:47:33 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Docker container has been started"
+echo "Docker конетнер запущен"
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /usr/local/cron/container.env
 
