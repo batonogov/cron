33d46088a87c04ceb36a3c2ffde3929bde814a08
Fedor Batonogov <batonogov@icloud.com>
2020-09-15 04:46:06 +0300

    Исправлена ошибка определения текущего времени

--- cron.hourly/cleaner.py
+++ cron.hourly/cleaner.py
@@ -2,12 +2,12 @@ import os, time
 
 
 path = '/var/log/cron'
-now = time.time()
 
 os.chdir(path)
 for root, dirs, files in os.walk('.', topdown = False):
    for name in files:
       file_name = os.path.join(root, name)
+      now = time.time()
       mtime = os.path.getmtime(file_name)
       if now - mtime > (7 * 86400):
           os.remove(file_name)
